1e048bc59e5fb59a35fd0e7b024b97d387bc182f
PANDAxDevV <108806900+PANDAxDevV@users.noreply.github.com>
2022-08-24 11:12:33 +0300

    Create ready.js

---
+++ event/ready.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+var oyun = [
+        "https://discord.gg/ec8U9NDKGa",
+        "Türkiye'nin yeni GIF botu!",
+        "g!yardım + g!davet"
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], `${client.users.size} Users` );
+        }, 2 * 2500);
+    
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+ console.log(`CodeBase uwu`);
+};
